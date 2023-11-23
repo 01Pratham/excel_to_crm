622e7e2098bf931e0583e526033e1ce589b5843e
01Pratham <cprathmesh01@gmail.com>
2023-11-23 19:16:29 +0530

    commit

--- temp.py
+++ temp.py
@@ -5,8 +5,20 @@ file = "template.xlsx"
 xl_file = pd.ExcelFile(file)
 
 SheetNames = xl_file.sheet_names
+group = []
+phase = []
+item = []
 
 for sheet in SheetNames:
-    df = pd.read_excel(file, sheet)
-    print (df)
+    if sheet == "VM Working" or sheet == "product_mater" :
+        continue 
+    df = pd.read_excel(file, sheet) 
+    group.append({sheet : []})
+    for index, row in df.iterrows():
+        if pd.notna(row['SKU']) and not pd.notna(row['Product Name']):
+            # group[sheet].append(row['SKU'])
+            print(type(group[1][sheet]))
+            continue
+        # print(row)
 
+print(group)
