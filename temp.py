3af4f4300f0998cfdd8cd01576976d4648ad4a36
01Pratham <cprathmesh01@gmail.com>
2023-11-25 23:12:32 +0530

    commit

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pprint import pprint
 import re
 
 file = "template.xlsx"
@@ -6,31 +7,88 @@ file = "template.xlsx"
 xl_file = pd.ExcelFile(file)
 
 SheetNames = xl_file.sheet_names
-group = []
-phase = []
-item = []
+result = []
 
 for sheet in SheetNames:
     if sheet == "VM Working" or sheet == "product_mater" :
         continue 
-    df = pd.read_excel(file, sheet) 
-    # print(df)
-    group.append({sheet : []})
-    phase.append({sheet : []})
-    for index, row in df.iterrows():
-        print (row)
-        if pd.notna(row['SKU']) and not pd.notna(row['Product Name']):
-            group[-1][sheet].append(row['SKU'])
-        else:
-            item
-            # continue
+    df = pd.read_excel(file, sheet)
+    for index , row in df.iterrows():
         keyList = list(row.keys())
         for key in keyList:
             if  key == "SKU" or  key == "Product Name":
                 continue
-    #         phase[-1][sheet].append(key)
-    # phase[-1][sheet] = list(set(phase[-1][sheet]))
+            result.append({key+" "+sheet : []})
+            
+unique_list = []
+unique_phase_keys = []
+for d in result:
+    if d not in unique_list:
+        unique_list.append(d)
+
+for dict in unique_list:
+    unique_phase_keys.append(list(dict.keys())[0])
     
+# print(unique_phase_keys)
+unique_group_keys = []
+
+for sheet in SheetNames:
+    if sheet == "VM Working" or sheet == "product_mater" :
+        continue 
+    df = pd.read_excel(file, sheet)
+    for index , row in df.iterrows():
+        for key in unique_phase_keys:
+            for i,val in enumerate(unique_list):
+                if pd.isna(row["Product Name"]):
+                    try:
+                        keyProduct = {row["SKU"] : []}
+                        unique_list[i][key].append(keyProduct)
+                    except KeyError:
+                        continue
+                    # print(unique_list[i][key])
+                try:
+                    for dict in unique_list[i][key]:
+                        unique_group_keys.append(list(dict.keys())[0])
+                except KeyError:
+                        continue
 
 
-# print(phase)
+unique_group_keys = list(set(unique_group_keys))
+
+newP = []
+
+for sheet in SheetNames:
+    if sheet == "VM Working" or sheet == "product_mater" :
+        continue 
+    df = pd.read_excel(file, sheet)
+    for index , row in df.iterrows():
+        for phase in unique_phase_keys:
+            phase_id = phase.replace(" "+sheet, '')
+            for i,val in enumerate(unique_list):
+                if not pd.isna(row["Product Name"]):
+                    for group in unique_group_keys:
+
+                        try:
+                            for j,gVal in enumerate(unique_list[i][key]):
+                                # print(phase_id)
+
+                                try:
+                                    unique_list[i][phase_id + " " + sheet][j][group].append({
+                                        "product_name" : row["Product Name"],
+                                        "product_sku" : row["SKU"],
+                                        "product_qty" : row[phase_id]
+                                    })
+                                    # newP.append({phase_id:row[phase_id]})
+                                except KeyError:
+                                    continue
+                        except KeyError:
+                            continue
+
+                # if not pd.isna(row["Product Name"]):
+
+
+
+print(unique_list)
+
+
+
