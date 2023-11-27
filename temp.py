2dc83c97e421ab97452abc5f380924f112a9cec3
01Pratham <cprathmesh01@gmail.com>
2023-11-27 17:54:46 +0530

    commit

--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from pprint import pprint
 import re
+import json
+import openpyxl as xl
+
 
 file = "template.xlsx"
 
@@ -8,87 +11,111 @@ xl_file = pd.ExcelFile(file)
 
 SheetNames = xl_file.sheet_names
 result = []
+def getFromCommanSheets():
+    for sheet in SheetNames:
+        if sheet == "VM Working" or sheet == "product_mater" :
+            continue 
+        df = pd.read_excel(file, sheet)
+        for index , row in df.iterrows():
+            # print(row)
 
-for sheet in SheetNames:
-    if sheet == "VM Working" or sheet == "product_mater" :
-        continue 
-    df = pd.read_excel(file, sheet)
-    for index , row in df.iterrows():
-        keyList = list(row.keys())
-        for key in keyList:
-            if  key == "SKU" or  key == "Product Name":
-                continue
-            result.append({key+" "+sheet : []})
-            
-unique_list = []
-unique_phase_keys = []
-for d in result:
-    if d not in unique_list:
-        unique_list.append(d)
+            keyList = list(row.keys())
+            for key in keyList:
+                if  key == "SKU" or  key == "Product Name" or key == "group":
+                    continue
+                result.append({key+" "+sheet : []})
+                
+    unique_list = []
+    unique_phase_keys = []
+    for d in result:
+        if d not in unique_list:
+            unique_list.append(d)
 
-for dict in unique_list:
-    unique_phase_keys.append(list(dict.keys())[0])
-    
-# print(unique_phase_keys)
-unique_group_keys = []
+    for dict in unique_list:
+        unique_phase_keys.append(list(dict.keys())[0])
+        
+    # print(unique_phase_keys)
+    unique_group_keys = []
 
-for sheet in SheetNames:
-    if sheet == "VM Working" or sheet == "product_mater" :
-        continue 
-    df = pd.read_excel(file, sheet)
-    for index , row in df.iterrows():
-        for key in unique_phase_keys:
-            for i,val in enumerate(unique_list):
-                if pd.isna(row["Product Name"]):
+    for sheet in SheetNames:
+        if sheet == "VM Working" or sheet == "product_mater" :
+            continue 
+        df = pd.read_excel(file, sheet)
+        for index , row in df.iterrows():
+            for key in unique_phase_keys:
+                for i,val in enumerate(unique_list):
+                    if pd.isna(row["Product Name"]):
+                        try:
+                            keyProduct = {row["SKU"] : []}
+                            unique_list[i][key].append(keyProduct)
+                        except KeyError:
+                            continue
+                        # print(unique_list[i][key])
                     try:
-                        keyProduct = {row["SKU"] : []}
-                        unique_list[i][key].append(keyProduct)
+                        for dict in unique_list[i][key]:
+                            unique_group_keys.append(list(dict.keys())[0])
                     except KeyError:
-                        continue
-                    # print(unique_list[i][key])
-                try:
-                    for dict in unique_list[i][key]:
-                        unique_group_keys.append(list(dict.keys())[0])
-                except KeyError:
-                        continue
+                            continue
 
 
-unique_group_keys = list(set(unique_group_keys))
+    unique_group_keys = list(set(unique_group_keys))
 
-newP = []
+    newP = []
 
-for sheet in SheetNames:
-    if sheet == "VM Working" or sheet == "product_mater" :
-        continue 
-    df = pd.read_excel(file, sheet)
-    for index , row in df.iterrows():
-        for phase in unique_phase_keys:
+    for sheet in SheetNames:
+        if sheet == "VM Working" or sheet == "product_mater" :
+            continue 
+        df = pd.read_excel(file, sheet)
+        for index , row in df.iterrows():
             for i,val in enumerate(unique_list):
-                for group in unique_group_keys:
-                    if row["SKU"] == group and pd.isna(row["Product Name"]):
+                phase = list(unique_list[i].keys())[0]
+                for g, group in enumerate(unique_list[i][phase]):
+                    if pd.isna(row["Product Name"]):
                         continue
                     try:
-                        for j,gVal in enumerate(unique_list[i][phase]):
-                            phase_id = phase.replace(" "+sheet, '')
-                            try:
-                                unique_list[i][phase][j][group].append({
+                        group = list(unique_list[i][phase][g].keys())[0]
+                        phase_id = phase.replace(" "+sheet, '')
+                        try:
+                            if group == row["group"]:
+                                unique_list[i][phase][g][group].append({
                                     "product_name" : row["Product Name"],
                                     "product_sku" : row["SKU"],
                                     "product_qty" : row[phase_id]
                                 })
-                            except KeyError:
-                                continue
+                        except KeyError:
+                            continue
                     except KeyError:
                         continue
-                   
-                # if not pd.isna(row["Product Name"]):
+    return unique_list
 
 
+                
 
+def getFromVmWorkingSheeet():
+    result = []
+    phases = []
+    test = []
+    wb = xl.load_workbook(filename=file)
+    for sheetName in wb.sheetnames:
+        if sheetName == "VM Working" :
+            sheetData = wb[sheetName]
+            for row in sheetData.iter_rows():
+                for cell in row:
+                    for merged_cell_range in sheetData.merged_cells.ranges:
+                        if cell.coordinate in merged_cell_range:
+                            if cell.value == None:
+                                continue
+                            if cell.value == "VM Details":
+                                continue
+                            result.append({cell.value : []})
+                            phases.append(cell.value)
+                        else:
+                            # if cell.value == None:
+                            #     continue
+                            # test.append(cell.value)
+                            continue
+                    test.append(cell.valu)
+                    
 
-# print(unique_list)
-
-a = open("test.txt","w+")
-a.write(str(unique_list))
-
-a.close()
+    print(test)
+getFromVmWorkingSheeet()
