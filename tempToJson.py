cbb11a54826e093eec49be7a883cacc2af00eb67
01Pratham <cprathmesh01@gmail.com>
2023-12-05 22:45:52 +0530

    commit

--- tempToJson.py
+++ tempToJson.py
@@ -1,7 +1,6 @@
 import temp  
 import pandas as pd
-import openpyxl
-from openpyxl.utils import get_column_letter
+import openpyxl as xl
 
 
 def getPhaseTenure(PhaseName):
@@ -39,6 +38,15 @@ products = list(set(products))
 tblHeaders = ["Phase Name", "Duration" , "Group Name", "Group Qty"]
 tblHeaders += products
 
+workbook = xl.Workbook()
+worksheet = workbook.active
 
+for col_num, header in enumerate(tblHeaders, 1):
+    worksheet.cell(row=1, column=col_num, value=header)
 
+for row_num, row_data in enumerate(phaseList, 2):
+    for col_num, cell_value in enumerate(row_data, 1):
+        worksheet.cell(row=row_num, column=col_num, value=cell_value)
 
+# Save the workbook
+workbook.save('crm_template.xlsx')
