4297b09978e2f9f0e0b146e941a669c2a9fdb638
01Pratham <cprathmesh01@gmail.com>
2023-12-08 16:14:32 +0530

    commit

--- app.py
+++ app.py
@@ -7,24 +7,16 @@ app = Flask(__name__)
 @app.route('/process', methods=['POST','GET'])
 def process():
     if request.method == 'POST':
-        # Get the uploaded file from the request
-        # return request.form
         uploaded_file = request.files['files']
-
-
         if uploaded_file:
-            # Save the file to a temporary location or your desired location
             file_path = os.path.join("uploads", "temp_uploaded_file.xlsx")
             uploaded_file.save(file_path)
-            # Process the uploaded file using DataProcessor or any other logic
             try : 
                 data_processor = DataProcessor(file_path)
                 data = data_processor.merged_dicts()
                 tbl_header = data_processor.tblHeaders()
                 phase_list = data_processor.getPhases()
                 phase_tenure = data_processor.phaseTenure()
-
-                # You can do something with the processed data, for example, render a template
                 return render_template("Process.html", data=data, header=tbl_header, Tenure=phase_tenure)
             except Exception as e:
                 return render_template("IncorrectFile.html")
